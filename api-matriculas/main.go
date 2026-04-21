859eee8c174b4d2d487c8b56051d9788f0e30906
Gui <leiturapipoca@gmail.com>
2026-04-20 23:53:06 -0300

    feat:conexao ao bd

--- api-matriculas/main.go
+++ api-matriculas/main.go
@@ -11,7 +11,6 @@ func main() {
 	fmt.Println("Iniciando serviços...")
 	database.InitDB()
 
-	// 2. Garante que a conexão com o banco feche graciosamente quando a API for desligada
 	defer database.Client.Close()
 	routes.SetupRoutes()
 	http.ListenAndServe(":8080", nil)
