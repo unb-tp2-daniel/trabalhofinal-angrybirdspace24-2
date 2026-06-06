25c7a0352e33646a2e2a1c87dcf3e79f868567fe
Guilherme Cavalcante <leiturapipoca@gmail.com>
2026-06-06 15:35:50 -0300

    cria novas turmas e materias p teste

--- backend/models/turma.go
+++ backend/models/turma.go
@@ -3,18 +3,18 @@ package models
 
 // Turma representa uma classe de uma matéria ofertada.
 // As tags `json` dizem como o frontend manda os dados.
-// As tags `firestore` dizem como o banco de dados vai salvar as chaves.
+// As tags `firestore` dizem como o banco de dados vai salvar as chavess.
 type Turma struct {
 	CodigoTurma     string            `json:"codigoTurma" firestore:"codigoTurma"`
 	MateriaId       string            `json:"materiaId" firestore:"materiaId"`
 	NomeMateria     string            `json:"nomeMateria" firestore:"nomeMateria"`
 	Semestre        string            `json:"semestre" firestore:"semestre"`
-	VagasTotais     int64               `json:"vagasTotais" firestore:"vagasTotais"`
-	VagasOcupadas   int64               `json:"vagasOcupadas" firestore:"vagasOcupadas"`
+	VagasTotais     int64             `json:"vagasTotais" firestore:"vagasTotais"`
+	VagasOcupadas   int64             `json:"vagasOcupadas" firestore:"vagasOcupadas"`
 	Ativo           bool              `json:"ativo" firestore:"ativo"`
 	Local           string            `json:"local" firestore:"local"`
 	Horario         string            `json:"horario" firestore:"horario"`
-	VagasExclusivas map[string]int64    `json:"vagasExclusivas" firestore:"vagasExclusivas"`
+	VagasExclusivas map[string]int64  `json:"vagasExclusivas" firestore:"vagasExclusivas"`
 	ProfessorId     string            `json:"professorId" firestore:"professorid"`
 	ProfessorNome   string            `json:"professorNome" firestore:"professorNome"`
 	Prioridades     map[string]string `json:"prioridades" firestore:"prioridades"`
